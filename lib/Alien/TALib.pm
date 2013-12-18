320acaecf8e328bd76ef003a6486bcc6d1e6b040
Vikas Naresh Kumar <vikas@cpan.org>
2013-12-18 05:04:15 +0000

    Import of VIKAS/Alien-TALib-0.01 from CPAN.
    gitpan-cpan-distribution: Alien-TALib
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         VIKAS/Alien-TALib-0.01.tar.gz
    gitpan-cpan-author:       VIKAS
    gitpan-cpan-maturity:     released

---
+++ lib/Alien/TALib.pm
@@ -0,0 +1,17 @@
+package Alien::TALib;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+use parent 'Alien::Base';
+
+
+1;
+
+__END__
+#### COPYRIGHT: Vikas N Kumar. All Rights Reserved
+#### AUTHOR: Vikas N Kumar <vikas@cpan.org>
+#### DATE: 17th Dec 2013
+#### LICENSE: Refer LICENSE file.
